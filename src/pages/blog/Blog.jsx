21731257d282f380a5c398ec0b7d2aa6d23341e3
akbor shanto <akborshanto2002@gmail.com>
2024-03-21 18:16:22 -0700

    theme

---
+++ src/pages/blog/Blog.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Blog = () => {
+  return (
+    <div>
+      THIS IS BLOG.....sdf
+    </div>
+  )
+}
+
+export default Blog
---
+++ src/pages/bookmark/Bookmar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Bookmar = () => {
+  return (
+    <div className='h-[300px]'>
+      Bookmark.....d
+      safsdlfks;lfs;
+    </div>
+  )
+}
+
+export default Bookmar
